734542ec4929df6c12911bed3dda4e5833b84355
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-22 21:43:32 +0330

    add parse method, load the xml

--- src/XmlCaptionParser.php
+++ src/XmlCaptionParser.php
@@ -7,4 +7,10 @@ class XmlCaptionParser
     public function __construct(
         protected string $path,
     ) {}
+
+    public function parse()
+    {
+        $file = file_get_contents($this->path);
+        $xml = simplexml_load_string($file);
+    }
 }
