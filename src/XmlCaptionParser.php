ca46f679d607a749e5b28ef5b85defdcb18acc75
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-22 21:45:50 +0330

    add lines and return caption

--- src/XmlCaptionParser.php
+++ src/XmlCaptionParser.php
@@ -8,9 +8,20 @@ class XmlCaptionParser
         protected string $path,
     ) {}
 
-    public function parse()
+    public function parse(): Caption
     {
         $file = file_get_contents($this->path);
         $xml = simplexml_load_string($file);
+        $caption = new Caption;
+
+        foreach ($xml->transcript as $text) {
+            $caption->add(new Line(
+                $text['start'],
+                $text['dur'],
+                $text,
+            ));
+        }
+
+        return $caption;
     }
 }
