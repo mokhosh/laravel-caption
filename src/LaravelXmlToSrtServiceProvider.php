0c4fa39f76d783fdeaeb9ab23b6c5c70073c2bf4
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-22 21:26:14 +0330

    clean up

--- src/LaravelXmlToSrtServiceProvider.php
+++ src/LaravelXmlToSrtServiceProvider.php
@@ -10,16 +10,9 @@ class LaravelXmlToSrtServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-xml2srt')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-xml2srt_table')
             ->hasCommand(LaravelXmlToSrtCommand::class);
     }
 }
