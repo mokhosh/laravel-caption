ff3b3aa698a9bc09b996876affeafe0ae945d141
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-22 21:59:01 +0330

    add srt generator

---
+++ src/SrtGenerator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Mokhosh\LaravelXmlToSrt;
+
+class SrtGenerator
+{
+    public function __construct(
+        protected Caption $caption,
+    ) {}
+
+    public function export(string $path)
+    {
+        $file = fopen($path, 'w');
+
+        foreach ($this->caption->lines as $index => $line) {
+            $output = 
+                $index + 1 . "\n" .
+                $line->start . " --> " .
+                $line->duration . "\n" .
+                $line->text . "\n\n";
+                
+            fwrite($file, $output);
+        }
+
+        fclose($file);
+    }
+}
