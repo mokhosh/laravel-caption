a920cf3ff5ea19a70a4715ce4a1406b4bdc2d444
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-23 14:20:02 +0330

    test timecode converter

---
+++ tests/TimecodeConverterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Mokhosh\LaravelXmlToSrt\TimecodeConverter;
+
+it('can convert float to timecode', function () {
+    $seconds = 0.0;
+    $timecode = TimecodeConverter::floatToTimecode($seconds);
+
+    expect($timecode)->toBe('00:00:00,000');
+
+    $seconds = 3.912;
+    $timecode = TimecodeConverter::floatToTimecode($seconds);
+
+    expect($timecode)->toBe('00:00:03,912');
+
+    $seconds = 162.561;
+    $timecode = TimecodeConverter::floatToTimecode($seconds);
+
+    expect($timecode)->toBe('00:02:42,561');
+});
