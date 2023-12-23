2964d16d52441b88ff2ef7fe782b57766089ca25
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-23 13:32:13 +0330

    initialize lines

--- src/Caption.php
+++ src/Caption.php
@@ -8,6 +8,11 @@ class Caption
 {
     protected Collection $lines;
 
+    public function __construct()
+    {
+        $this->lines = collect();
+    }
+
     public function add(Line $line)
     {
         $this->lines->push($line);
