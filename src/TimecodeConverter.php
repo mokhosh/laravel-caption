1ae0f1c9ced88ebb52f708df4aeba5a0bc95a579
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-23 14:19:42 +0330

    add timecode converter

---
+++ src/TimecodeConverter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mokhosh\LaravelXmlToSrt;
+
+use DateTime;
+use DateTimeZone;
+
+class TimecodeConverter
+{
+    public static function floatToTimecode(float $seconds): string
+    {
+        $date = DateTime::createFromFormat('U.u', sprintf("%.3f", $seconds));
+        $date->setTimezone(new DateTimeZone('UTC'));
+
+        return $date->format("H:i:s,v");
+    }
+}
