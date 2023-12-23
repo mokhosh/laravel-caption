12ed297e80b919149b1a419a2e259ac31abf40d6
Mo Khosh <mskhoshnazar@gmail.com>
2023-12-23 13:33:31 +0330

    add basic tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,19 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use Mokhosh\LaravelXmlToSrt\SrtGenerator;
+use Mokhosh\LaravelXmlToSrt\XmlCaptionParser;
+
+it('can read xml caption files', function () {
+    $caption = XmlCaptionParser::import('tests/test.xml')->parse();
+
+    expect($caption->lines()->count())->toBe(9);
+});
+
+it('can generate srt files', function () {
+    $parser = new XmlCaptionParser('tests/test.xml');
+    $caption = $parser->parse();
+    $generator = new SrtGenerator($caption);
+    $generator->export('tests/test.srt');
+
+    expect('tests/test.srt')->toBeReadableFile();
 });
