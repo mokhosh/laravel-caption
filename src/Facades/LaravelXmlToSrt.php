e019379825eee5a0c23ac29907032dc37a9f9bce
Mo Khosh <mskhoshnazar@gmail.com>
2024-01-07 16:43:20 +0330

    add facade docs

--- src/Facades/LaravelXmlToSrt.php
+++ src/Facades/LaravelXmlToSrt.php
@@ -5,6 +5,9 @@ namespace Mokhosh\LaravelXmlToSrt\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string convert(string $input, string $output)
+ * @method static \Illuminate\Support\Collection chunk(string $input, int $every, string $outputFolder, string $prefix)
+ *
  * @see \Mokhosh\LaravelXmlToSrt\LaravelXmlToSrt
  */
 class LaravelXmlToSrt extends Facade
