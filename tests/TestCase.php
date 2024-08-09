19c32f23d7f80ae1cf305d846264ffe3106a3f77
Mo Khosh <mskhoshnazar@gmail.com>
2024-08-09 12:31:41 +0330

    remove unused stuff

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,15 @@
 
 namespace Mokhosh\LaravelCaption\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Mokhosh\LaravelCaption\LaravelCaptionServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Mokhosh\\LaravelCaption\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             LaravelCaptionServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-caption_table.php.stub';
-        $migration->up();
-        */
     }
 }
